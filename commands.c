87bfd82c93296b6418ac123d6151e45d86d01e70
raviarhaa <ninenowlovenature999st.gas@gmail.com>
2023-08-18 17:08:39 +0530

    commands

---
+++ commands.c
@@ -0,0 +1,10 @@
+open editor by type : vim "filename"
+after save the file.
+git add 'filename'
+git commit -m "give msg"
+git remote add origin give path
+if you got any error 
+ do these git fetch origin master
+ and 
+ git merge origin/master
+ git push origin master
